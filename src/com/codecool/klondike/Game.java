ac86519b79ac1b4ab8f8f4bcc2f7dae01b445d82
Gergely Esztojka <esztojka.gergely@gmail.com>
2019-01-02 14:09:26 +0100

    Starting tableau feature is done

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -180,6 +181,21 @@ public class Game extends Pane {
         }
     }
 
+    public void fillTableauPiles() {
+
+        Card card;
+
+        for (int i = 0; i < tableauPiles.size(); i++) {
+            for (int j = 0; j < i+1; j++) {
+                card = stockPile.getTopCard();
+                card.moveToPile(tableauPiles.get(i));
+                if(j ==i ) {
+                    card.flip();
+                }
+            }
+        }
+    }
+
     public void dealCards() {
         Iterator<Card> deckIterator = deck.iterator();
         //TODO
@@ -188,7 +204,7 @@ public class Game extends Pane {
             addMouseEventHandlers(card);
             getChildren().add(card);
         });
-
+        fillTableauPiles();
     }
 
     public void setTableBackground(Image tableBackground) {
